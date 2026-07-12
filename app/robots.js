34a7c27ab41590da39373630691368dc195b52ab
amanpandit841404-eng <amanpandit841404@gmail.com>
2026-07-12 07:59:07 +0530

    SEO: Enhanced robots.txt with better crawl rules

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,12 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/api/', '/quick-add', '/_next/'],
+      },
+    ],
     sitemap: 'https://exam-platform-beta.vercel.app/sitemap.xml',
   };
 }
